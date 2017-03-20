89325bd4ce6a0b600604a97fd3d124e3041a3b8f
Hendrik Lösch <loeschhendrik@googlemail.com>
2017-03-20 08:46:40 +0100

    Components

--- src/pages/favorite-collection/favorite-collection.ts
+++ src/pages/favorite-collection/favorite-collection.ts
@@ -12,31 +12,6 @@ export class FavoriteCollectionPage implements OnInit {
   constructor(private store : FavoriteStore) { }
 
   ngOnInit() {
-    //this.favorites = this.store.getFavorites();
-
-    this.createTestData();      
+    this.favorites = this.store.getFavorites();       
   }
-
-  private createTestData() {
-        let favorite =  {
-         "Title":"The Test",
-         "Year": 2012,
-         "imdbID":"tt1986180",
-         "Comment":"movie",
-         "Poster":"https://images-na.ssl-images-amazon.com/images/M/MV5BMTYwNTgzMjM5M15BMl5BanBnXkFtZTcwNDUzMTE1OA@@._V1_SX300.jpg"
-      };
-
-      this.favorites.push(favorite)
-    
-      favorite = {
-          "Title":"Rabbit Test",
-          "Year": 1978,
-          "imdbID":"tt0078133",
-          "Comment": "another movie",
-          "Poster":"https://images-na.ssl-images-amazon.com/images/M/MV5BMTI1MDEwNDI5Ml5BMl5BanBnXkFtZTYwNTQ1Mjg5._V1_SX300.jpg"
-        };
-
-      this.favorites.push(favorite)
-  }
-
 }
